90ce5142b152e944e8467c3617d700477805b6d8
Tatyana <tanyagta@bk.ru>
2019-10-24 17:47:20 +0300

    Updates

---
+++ precache-manifest.46cd394a40fd8876430e9557c2e98135.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc25fdc7763fae5d1747142f75db4ac6",
+    "url": "/react-game/index.html"
+  },
+  {
+    "revision": "a84aea2ae01aac16f884",
+    "url": "/react-game/static/css/main.acb2ade7.chunk.css"
+  },
+  {
+    "revision": "b7ec751d625323be5ae0",
+    "url": "/react-game/static/js/2.d17959ea.chunk.js"
+  },
+  {
+    "revision": "a84aea2ae01aac16f884",
+    "url": "/react-game/static/js/main.db1bdd81.chunk.js"
+  },
+  {
+    "revision": "d312ff7d0a2585cf54bf",
+    "url": "/react-game/static/js/runtime-main.d5548149.js"
+  }
+]);
